c209368c493ea6c923da88853289781653c05938
shreyashkumarsingh <shreyashkrsingh@gmail.com>
2025-07-15 12:08:45 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
@@ -19,9 +18,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "src"),
